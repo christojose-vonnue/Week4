06aafc0c7712215f03a747e60964724856a9395c
Christo Jose <christo.jose@vonnue.com>
2026-09-03 09:20:51 +0530

    Day 3 : Task 2 is completed

--- utils.js
+++ utils.js
@@ -74,6 +74,149 @@ function partial(fn, ...presetArgs) {
     };
 }
 
+// Task 2 Utils.js
+
+
+class EventEmitter{
+    constructor(){
+        this.events={}
+    }
+    on(event, listener){
+        // as you said event is a label 
+        // listners are simple functions
+        // I would assume we need to add this as a key-value pair to events
+
+        // Version 1 :  I was wrong, on means just add a listner to the functions, if not add new event
+        // let temp
+        // temp={event:listener}
+        // Object.assign(this.events,temp)
+
+        if(!(this.events[event])){
+            this.events[event] = [];
+        }
+        // Improved , we pre-checked if event exist,  if not we create and push , if yes we push
+        // for(let i of Object.keys(this.events)){
+        //     if(event==i){
+                this.events[event].push(listener)
+                return
+        //     }
+        // }
+
+    }
+
+    off(event,listener){
+        // I would assume we need to remove this event as we dont need it,
+        // So check if the event exist in events directory and if yes remove if
+
+        // Version 3
+        if (!this.events[event]) return;
+        const index=this.events[event].indexOf(listener)
+        if(index!==-1){
+            this.events[event].splice(index,1)
+        }
+
+        // Version 2, is correct, the above Version 3 it much simpler code
+        // for(let i of Object.keys(this.events)){
+        //     if(event==i){
+        //         let curr_listners=this.events[event]
+        //         for(i of curr_listners){
+        //             if(i==listener){
+        //                 const index = curr_listners.indexOf(listener)
+        //                 this.events[event].splice(index,1)
+        //                 return
+        //             }
+        //         }
+        //     }
+        // }
+    }
+
+    //emit(event, ...args): Calls all listeners registered under event (AND any wildcards registered under '*')
+    // I think emit means to actually call all the listeners stored under an event..
+    // args must be the callback functions inside them , also if args= "*".. call all the listeners
+
+    emit(event, ...args){
+        if(this.events[event]) {
+            // VERSION 2 ... args are payload data, * is a wildcard function, a special event as i understand
+            for(let i of this.events[event]){
+                i(...args)
+            }
+        }
+            // I believe that wildcard listners must always run , if they exist in the events
+            if(this.events["*"]){
+                this.events['*'].forEach(listener => listener(event, ...args));
+            }
+
+            //check if the event actually exist
+
+        //check if * is in the array args, if yes, execute all listners VERSION 1
+        // if(args.includes("*")){
+        //     this.events[event].forEach(listener => {
+        //         listener()
+        //     });
+        //     return
+        // }
+        // else{
+        //     for (let i of args){
+        //         if(this.events[event].includes(i)){
+        //             i() // I asssume i are listeners.. listeners are functions and we are calling them..??
+        //         }
+                
+        //     }
+        // }
+
+    }
+
+    once(event,listener){ //once must automatically remove the listener after it fires
+        // if(!(this.events[event])) return
+
+        // if(this.events[event].includes(listener)){
+        //     listener()
+        // } // call the listner)
+        // this.off(event,listener)
+
+        // Version 2
+        // Create a function object that calls the listener and removes it
+        const functionobject=(...args)=>{
+            listener(...args)
+            this.off(event,functionobject) //self removal
+        }
+        
+        // Step 2 add it to the emitter
+        this.on(event,functionobject)
+    }
+}
+
+ async function fetchdata(url) {               
+        const response =await fetch(url)  // "https:/jsonplaceholder.typicode.com/posts"
+        if(!(response.ok)){
+            if(response.status==404){
+                console.warn("wrong url")
+                throw new Error("wrong url")
+            }
+            else if(response.status>=300){
+                console.warn("Error 300+")
+                throw new Error("check response status")
+            }
+        }
+        return await response.json()
+    }
+
+async function fetchWithRetry(url,retires=1) {
+    for(let i=0;i<2;i++){
+        try{
+            const res=await fetch(url)
+            if(!res.ok){
+                throw new Error('HTTP Error')
+            }
+            return await res.json()
+        }catch(error){
+            if(i==retires){
+                throw new Error('HTTP Error')
+            }
+        }
+    }
+}
+
 // I would like another blueprint, this time less abstract
 chunk([1,2,3,1,2,3],2)
 // let testarr=[1,23,4,5,6,7,8,9]
@@ -86,6 +229,10 @@ module.exports = {
   pipe,
   compose,
   curry,
-  partial
+  partial,
+  //Task 2 is exported here
+  EventEmitter,
+  fetchdata,
+  fetchWithRetry
 };
 
--- utils.test.js
+++ utils.test.js
@@ -1,5 +1,5 @@
 // import { chunk, zip, groupBy }  from "./utils.js"
-const { chunk, zip, groupBy, pipe, compose,curry,partial } = require("./utils");
+const { chunk, zip, groupBy, pipe, compose,curry,partial ,EventEmitter,fetchdata,fetchWithRetry} = require("./utils");
 
 test('chunk test', () => {
     //Happy Path
@@ -66,3 +66,82 @@ test('partial',()=>{
     const result=testcase1(3)
     expect(result).toBe(6)
 })
+
+// Task 2 Files
+
+describe('EventEmitter module',()=>{
+    //Write various test functions
+    const initalEventEmitter=new EventEmitter()
+    test('Argument & Execution Verification',()=>{
+        const mockListener=jest.fn()
+        initalEventEmitter.on("userlogin",mockListener)
+        initalEventEmitter.emit('userlogin',{"id":42},"admin")
+        expect(mockListener).toHaveBeenCalled()
+        expect(mockListener).toHaveBeenCalledWith({"id":42},"admin")
+        mockListener
+
+        const mockListener1=jest.fn()
+        const mockListener2=jest.fn()
+        initalEventEmitter.on("common",mockListener1)
+        initalEventEmitter.on("common",mockListener2)
+        initalEventEmitter.emit('common',{"id":42},"admin")
+        expect(mockListener1).toHaveBeenCalledTimes(1)
+        expect(mockListener2).toHaveBeenCalledTimes(1)
+
+    })
+})
+
+describe('fetchjson',()=>{
+    afterEach(()=>{jest.restoreAllMocks();})
+    test('Success path', async()=>{
+
+        jest.spyOn(global,'fetch').mockResolvedValue({
+            ok : true,
+            json : jest.fn().mockResolvedValue({data:"success"})
+        })
+        const data=await fetchdata("https://api.example.com/data")
+        expect(data).toEqual({data:"success"})
+        expect(global.fetch).toHaveBeenCalledWith("https://api.example.com/data")
+    })
+
+    test('Failure path',async()=>{
+        jest.spyOn(global,'fetch').mockResolvedValue({
+            ok:false,
+            status:404
+        })
+
+        await expect(fetchdata("https://api.example.com/data")).rejects.toThrow("wrong url")
+    })
+
+    test("Failed Network",async()=>{
+        jest.spyOn(global,'fetch').mockRejectedValue(new Error('Network Failure'))
+
+        await expect(fetchdata("https://api.example.com/data")).rejects.toThrow('Network Failure')
+    })
+    // test('Failure path', async()=>{
+    //     jest.spyOn(()=>{jest.restoreAllMocks();})
+
+    //     jest.spyOn(global,'fetch')
+    // })
+    // await except(fetchdata("https://api.example.com/data")).rejects.toThrow("HTTP Error 404")
+})
+
+describe('fetchWithRetry',()=>{
+    afterEach(()=>{
+        console.log("Cleared before next test");
+        jest.restoreAllMocks()  
+    })
+
+    test('Retry..',async ()=>{
+        jest.spyOn(global,'fetch')
+        .mockRejectedValueOnce(new Error("Network TRansient Error"))
+        .mockResolvedValueOnce({
+            ok:true,
+            json:jest.fn().mockResolvedValue({data:'retry_sucesss'})
+        })
+
+        const result=await fetchWithRetry("https://api.example.com/retry",2)
+        expect(result).toEqual({data:'retry_sucesss'})
+    })
+})
+
